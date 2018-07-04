c313f3adbb7d638cfcb5d403e8d55c424b87a515
alxdev91 <40773006+alxdev91@users.noreply.github.com>
2018-07-03 19:41:17 -0500

    DemoMvc -list student

---
+++ DemoMvc/Controllers/PersonaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMvc.Models;
+
+namespace DemoMvc.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            Persona persona = new Persona();
+            persona.Name = "Alex";
+            persona.Edad = 27;
+            persona.Id = 1127;
+            persona.Genero = "H";
+            return View(persona);
+        }
+    }
+}
---
+++ DemoMvc/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using DemoMvc.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            //El resultado del metodo Estudiantes lo asigno al ListadoEst que es de tipo list que acepta valor tipo student
+            List<Student> ListadoEst = Estudiantes();
+
+
+            return View(ListadoEst);
+        }
+
+        public List<Student> Estudiantes()
+        {    
+            //Creo una lista 
+            List<Student> Est = new List<Student>();
+
+            //Asigno valores a la lista
+            Est.Add(new Student { Name = "Alex", Semester = 8 });
+            Est.Add(new Student { Name = "B", Semester = 38 });
+            Est.Add(new Student { Name = "C", Semester = 85 });
+
+            //Regreso la lista con los valores
+            return Est;
+        }
+        
+    }
+}
